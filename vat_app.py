29446089245b45730d3fdd502aae217bd9eabf0e
403-01-Pattarawadee <54124@yupparaj.ac.th>
2026-08-03 13:21:12 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -6,5 +6,6 @@ vat = price * 0.07
 net_price = price - vat
 
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
 st.write("นางสาว ภัทรวดี สุขสวัสดิ์ เลขที่ 1  ม.4/3")
